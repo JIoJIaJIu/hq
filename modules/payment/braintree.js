723fe0b841643014c1b238ec828d10532c30559e
Guro Bokum <jiojiajiu@gmail.com>
2015-09-17 23:47:28 +0300

    braintree

--- modules/payment/braintree.js
+++ modules/payment/braintree.js
@@ -1,10 +1,128 @@
+var Q = require('q');
+var braintree = require('braintree');
+var logger = require('winston');
+
+var config = require('config');
+var utils = require('../utils');
+
+var ERROR_CODES = config.get('ERROR_CODES');
+
 function Braintree () {
+    config = config.get('payments.braintree');
+
+    this._customers = {};
+    this._gateway = braintree.connect({
+        environment: braintree.Environment.Sandbox,
+        merchantId: config.merchantId,
+        publicKey: config.publicKey,
+        privateKey: config.privateKey
+    });
 }
 
 Braintree.prototype = {
     createPayment: function (data) {
-        return null, true
-    }
+        var self = this;
+        var def = Q.defer();
+        var customerId = utils.generateCustomerId(data.firstName, data.lastName);
+
+        this._getCustomer(customerId, data)
+            .then(function (customer) {
+                self._gateway.transaction.sale({
+                    amount: data.price,
+                    paymentMethodNonce: data.nonce
+                }, function (err, response) {
+                    if (response.errors) {
+                        def.reject(utils.Error(ERROR_CODES.SERVER_ERROR, response.message));
+                        return;
+                    }
+
+                    def.resolve(response.id);
+                })
+            });
+
+        return def.promise 
+    },
+
+    getClientToken: function (data) {
+        var self = this;
+        var def = Q.defer();
+        var customerId = utils.generateCustomerId(data.firstName, data.lastName);
+        logger.info("[Braintree] Getting client token %s", customerId)
+
+        this._getCustomer(customerId, data)
+            .then(function (customer) {
+                self._gateway.clientToken.generate({
+                    customerId: customer.id
+                }, function (err, response) {
+                    if (response.errors) {
+                        logger.error("[Braintree] Error during generate client token");
+                        def.reject(utils.Error(ERROR_CODES.SERVER_ERROR, response.message));
+                        return;
+                    }
+
+                    def.resolve(response.clientToken);
+                });
+            });
+
+        return def.promise;
+    },
+
+    _getPaymentMethod: function (customer, nonce, data) {
+        var self = this;
+        var def = Q.defer();
+        var token = utils.generateCardHash(data.cardNumber, data.ccv);
+        logger.info("[Braintree] Getting credit card %s", token)
+
+        self._gateway.paymentMethod.create({
+            customerId: customer.id,
+            paymentMethodNonce: nonce
+        }, function (err, paymentMethod) {
+            console.log(6, err, paymentMethod);
+            if (err) {
+                logger.error("[Braintree] Error during creating payment method")
+                def.reject(err)
+                return;
+            }
+
+            def.resolve(paymentMethod);
+        })
+
+        return def.promise
+    },
+
+    _getCustomer: function (id, data) {
+        logger.info("[Braintree] Getting customer %s", id)
+        var def = Q.defer();
+        var self = this;
+
+        this._gateway.customer.find(id, function (err, customer) {
+            if (customer) {
+                self._customers[id] = customer;
+                def.resolve(customer);
+                return;
+            }
+
+            self._gateway.customer.create({
+                id: id,
+                firstName: data.firstName,
+                lastName: data.lastName
+            }, function (err, customer) {
+                if (err) {
+                    logger.error("[Braintree] Error during creating customer")
+                    def.reject(err)
+                    return;
+                }
+
+                self._customers[id] = customer;
+                def.resolve(customer);
+            });
+
+        });
+
+        return def.promise
+    },
+
+    _customers: null
 }
 
 module.exports = {
---
+++ test/braintree.js
@@ -0,0 +1,29 @@
+var braintree = new (require('../modules/payment/braintree.js').Braintree)()
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000
+
+describe("Braintree payment", function () {
+    it("get client token", function (done) {
+        braintree.getClientToken({
+            firstName: "Joe",
+            lastName: "Shopper"
+        }).then(function (a) {
+            expect(true).toBe(true);
+        }, function (error) {
+            expect(true).toBe(false);
+        }).fin(done);
+    });
+
+    it("valid payment", function (done) {
+        braintree.createPayment("fake-valid-nonce", {
+            firstName: "Joe",
+            lastName: "Shopper",
+            amount: 100,
+            currency: "USD"
+        }).then(function () {
+            expect(true).toBe(true);
+        }, function () {
+            expect(true).toBe(false);
+        }).fin(done);;
+    });
+});
