b1882070ebccf1954a0f45d9057378e72d557b10
Guro Bokum <jiojiajiu@gmail.com>
2015-09-18 00:14:36 +0300

    remove test from default

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var exec = require('child_process').exec
 var jasmine = require('gulp-jasmine');
 
-gulp.task('default', ['deps', 'test']);
+gulp.task('default', ['deps']);
 
 gulp.task('test', function () {
     return gulp.src('test/**.js').pipe(jasmine());
